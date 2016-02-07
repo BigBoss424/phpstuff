8cb606e1a227371b36501c74458507a8ff261491
BigBoss424 <ajones061392@gmail.com>
2016-02-07 02:50:33 -0500

    Started work on learning the different datatypes for PHP.

---
+++ phpdatatypes.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ Title: PHP Datatypes 
+ Author: Aaron Jones
+ Date: 2/6/16
+*/
+
+//Boolean 
+$alive = false; #$alive is false.
+$alive = 1; #$alive is true.
+$alive = -1; #$alive is true.
+$alive = 5; #$alive is true.
+$alive = 0; #alive is false.
+
+//Integer 
+42	#decimal 
+-678900 #decimal
+0755	#octal
+0xC4E	#hexadecimal
+
+//Maximum supported integer
+$val = 45678945939390393678976; 
+echo $val + 5;
+
+//Float 
+$floatnumber = 4.5678;
+$floatnumber1 = 4.0;
+$floatnumber2 = 8.7e4;
+$floatnumber3 = 1.23E+11;
+
+//String
+$string1 = "whoop-de-do";
+$string2 = 'subway\n';
+$string3 = "123$%^789";
+
+
+
+?>
